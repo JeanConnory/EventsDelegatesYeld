569d8073d88c877a946384d831a5032a05324c32
jean.connory <jean.connory@outlook.com>
2023-10-11 21:35:41 -0300

    yield finished

---
+++ UsingYeld/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var numbers = GetEvenNumbers(1000000000); //1 Bilion
+
+foreach (var number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+static IEnumerable<int> GetEvenNumbers(int max)
+{
+    //List<int> numbers = new List<int>();
+
+    for (int i = 0; i < max; i++)
+    {
+        if(i % 2 == 0)
+        {
+            //numbers.Add(i);
+            yield return i;
+        }
+    }
+
+    //return numbers;
+}
