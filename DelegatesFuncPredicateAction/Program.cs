149d40e989cbee1dc6cb5fe8a2db06f10182d0bd
jean.connory <jean.connory@outlook.com>
2023-10-13 13:41:31 -0300

    Predicate

--- DelegatesFuncPredicateAction/Program.cs
+++ DelegatesFuncPredicateAction/Program.cs
@@ -34,32 +34,76 @@
 
 #region Func
 
-float media;
-//media = CalculaMedia(3, 5);
+//float media;
+////media = CalculaMedia(3, 5);
 
-defFuncDelegate f;
-f = CalculaMedia;
-media = f(8, 10);
+//defFuncDelegate f;
+//f = CalculaMedia;
+//media = f(8, 10);
 
-defFuncDelegateGenerico<int, int, float> f1;
-f1 = CalculaMedia;
-media = f1(5, 9);
+//defFuncDelegateGenerico<int, int, float> f1;
+//f1 = CalculaMedia;
+//media = f1(5, 9);
 
-Func<int, int, float> f2; //Forma mais fácil e padrão de usar
-f2 = CalculaMedia;
-media = f2(6, 4);
+//Func<int, int, float> f2; //Forma mais fácil e padrão de usar
+//f2 = CalculaMedia;
+//media = f2(6, 4);
 
-Console.WriteLine($"Média: {media}");
+//Console.WriteLine($"Média: {media}");
 
-static float CalculaMedia(int p1, int p2)
+//static float CalculaMedia(int p1, int p2)
+//{
+//    int media;
+//    media = (p1 + p2) / 2;
+//    return media;
+//}
+
+//delegate float defFuncDelegate(int p1, int p2);
+
+//delegate TResult defFuncDelegateGenerico<in T1, in T2, out TResult>(T1 p1, T2 p2);
+
+#endregion
+
+#region Predicate
+
+bool resultado;
+//resultado = DeterminaNumeroPar(2);
+
+Predicate<int> p;
+p = DeterminaNumeroPar;
+//resultado = p(2);
+//Console.WriteLine($"Resultado: {resultado}");
+
+List<int> listaCompleta = new List<int>
 {
-    int media;
-    media = (p1 + p2) / 2;
-    return media;
+    10,
+    11,
+    12,
+    13,
+    14,
+    15
+};
+
+List<int> listaDosPares = new List<int>();
+listaDosPares = listaCompleta.FindAll(p);
+
+ImprimeLista(listaDosPares);
+
+static bool DeterminaNumeroPar(int num)
+{
+    return num % 2 == 0;
 }
 
-delegate float defFuncDelegate(int p1, int p2);
+static void ImprimeLista(List<int> lista)
+{
+    for (int i = 0; i < lista.Count; i++)
+    {
+        Console.WriteLine(lista[i]);
+    }
+}
 
-delegate TResult defFuncDelegateGenerico<in T1, in T2, out TResult>(T1 p1, T2 p2);
+//delegate bool Predicate(int num);
+
+//delegate bool Predicate<in T>(T num); Já existe um Predicate padrão
 
 #endregion
